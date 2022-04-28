50b17ba709037478ec65f45b23965fe5361bd724
ON21MB <mar.bauer.21@lehre.mosbach.dhbw.de>
2022-04-28 19:01:46 +0200

    Implemented and styled button

---
+++ src/components/jplants-button/jplants-button.tsx
@@ -0,0 +1,34 @@
+import { Component, Host, h, Prop} from '@stencil/core';
+
+@Component({
+  tag: 'jplants-button',
+  styleUrl: 'jplants-button.css',
+  shadow: true,
+})
+export class JplantsButton {
+
+@Prop() buttonName: string;
+@Prop() buttonHref: string;
+
+willShowButton(){
+  if(this.buttonName && this.buttonHref){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+  render() {
+    return <Host>
+      {
+        this.willShowButton() && 
+        <a href="#" class="btn">
+          <button type="button" name="button">
+          {this.buttonName}
+          </button>
+        </a>
+      }
+      </Host>
+  }
+
+}
