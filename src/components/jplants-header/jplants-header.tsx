fd20129200c7055c2826f683fef07cc624776517
ON21MB <mar.bauer.21@lehre.mosbach.dhbw.de>
2022-04-28 19:07:12 +0200

    Implemented and styled header

---
+++ src/components/jplants-header/jplants-header.tsx
@@ -0,0 +1,51 @@
+import { Component, Host, h, Prop, State, Watch, getAssetPath } from '@stencil/core';
+
+@Component({
+  tag: 'jplants-header',
+  styleUrl: 'jplants-header.css',
+  shadow: true,
+  assetsDirs: ['assets']
+})
+export class JplantsHeader {
+@Prop() navLinksJson: string;
+@Prop() logo = "jplants-logo.jpg";
+@Prop() menuSymbol: string = "bi bi-list";
+
+@State() navLinks: Array<{iconClass: string, name: string, href: string}> = [];
+
+
+@Watch('navLinksJson')
+handleNavLinksJsonChanged(){
+  this.convertNavJson();
+}
+
+componentWillLoad(){
+  this.convertNavJson();
+}
+
+convertNavJson(){
+  this.navLinks = JSON.parse(this.navLinksJson);
+}
+
+  render() {
+    const imageSrc = getAssetPath(`./assets/${this.logo}`);
+    return (
+      <Host>
+        <div class="header">
+        <i class={this.menuSymbol}></i>
+        <img src={imageSrc} />
+        <nav>
+          <ul class="nav">
+            {this.navLinks.map(navLink => (
+              <li>
+                <a href={navLink.href}><i class={navLink.iconClass}></i>{navLink.name}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        </div>
+      </Host>
+    );
+  }
+
+}
