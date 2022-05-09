7e8fe86421ae97f84c50845f290f862208dd16b9
Katharina Boni <katharina.boni@ueberbit.de>
2022-05-09 21:01:12 +0200

    Deleted unused Code

--- src/components/jplants-rating-stars/jplants-rating-stars.tsx
+++ src/components/jplants-rating-stars/jplants-rating-stars.tsx
@@ -6,9 +6,7 @@ import { Component, Host, h, Prop } from '@stencil/core';
   shadow: true,
 })
 export class JplantsRatingStars {
-
   @Prop() filledStars: number;
-  // @Prop() emptyStars: number;
   filledStarArray: string[] = [];
   emptyStarArray: string[] = [];
   filledStarsAsString: string;
@@ -16,26 +14,29 @@ export class JplantsRatingStars {
 
   componentWillLoad(currentFilled: number) {
     currentFilled = this.filledStars;
-    
-    if(currentFilled <= 5) {
-    for (let i = 0; i < currentFilled; i++) {
-      this.filledStarArray.push('★');
-    } this.filledStarsAsString = this.filledStarArray.join('');
-  
-    for (let i = 0; i < (5-currentFilled); i++) {
-      this.emptyStarArray.push('★');
-    } this.emptyStarsAsString = this.emptyStarArray.join('');
 
-    return this.emptyStarsAsString, this.filledStarsAsString;
-  } else {
-    for (let i = 0; i < 5; i++) {
-      this.filledStarArray.push('★');
-    } this.filledStarsAsString = this.filledStarArray.join('');
-    console.log('The number of stars exceeded the maximum of 5.')
-} 
+    if (currentFilled <= 5) {
+      for (let i = 0; i < currentFilled; i++) {
+        this.filledStarArray.push('★');
+      }
+      this.filledStarsAsString = this.filledStarArray.join('');
+
+      for (let i = 0; i < 5 - currentFilled; i++) {
+        this.emptyStarArray.push('★');
+      }
+      this.emptyStarsAsString = this.emptyStarArray.join('');
+
+      return this.emptyStarsAsString, this.filledStarsAsString;
+    } else {
+      for (let i = 0; i < 5; i++) {
+        this.filledStarArray.push('★');
+      }
+      this.filledStarsAsString = this.filledStarArray.join('');
+      console.log('The number of stars exceeded the maximum of 5.');
+    }
   }
 
-  connectedCallback(){
+  connectedCallback() {
     this.filledStarArray = [];
     this.emptyStarArray = [];
   }
@@ -44,8 +45,8 @@ export class JplantsRatingStars {
     return (
       <Host>
         <div class="rating-stars">
-        <p id="rating-stars-filled">{this.filledStarsAsString}</p>
-        <p id="rating-stars-empty">{this.emptyStarsAsString}</p>
+          <p id="rating-stars-filled">{this.filledStarsAsString}</p>
+          <p id="rating-stars-empty">{this.emptyStarsAsString}</p>
         </div>
       </Host>
     );
