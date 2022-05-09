27075037a0091fcb6a4e5dc16da73ab2fb28b743
ON21MB <mar.bauer.21@lehre.mosbach.dhbw.de>
2022-05-09 18:48:16 +0200

    Finalized rating-tool-logic

--- src/components/jplants-rating-stars/jplants-rating-stars.tsx
+++ src/components/jplants-rating-stars/jplants-rating-stars.tsx
@@ -14,29 +14,22 @@ export class JplantsRatingStars {
   @Prop() filledStarsAsString: string;
   @Prop() emptyStarsAsString: string;
 
-  componentWillLoad(currentFilled: number) {
-    // this.displayFilledStars;
-    // this.displayEmptyStars;
+  componentWillLoad(currentFilled: number, currentEmpty: number) {
     currentFilled = this.filledStars;
     for (let i = 0; i < currentFilled; i++) {
       this.filledStarArray.push('★');
     } this.filledStarsAsString = this.filledStarArray.join('');
-    return this.filledStarsAsString
-  } 
-
- //displayFilledStars(currentFilled: number) {}
-    
-/* 
- displayEmptyStars(currentEmpty: number) {
+  
     currentEmpty = this.emptyStars;
 
     for (let i = 0; i < currentEmpty; i++) {
       this.emptyStarArray.push('★');
     } this.emptyStarsAsString = this.emptyStarArray.join('');
-    this.emptyStarsAsString;
-  } */
 
-  componentDidUpdate(){
+    return this.emptyStarsAsString, this.filledStarsAsString;
+  } 
+
+  connectedCallback(){
     this.filledStarArray = [];
     this.emptyStarArray = [];
   }
@@ -46,7 +39,7 @@ export class JplantsRatingStars {
       <Host>
         <div class="rating-stars">
         <p id="rating-stars-filled">{this.filledStarsAsString}</p>
-        {/* <p id="rating-stars-empty">{this.emptyStarsAsString}</p> */}
+        <p id="rating-stars-empty">{this.emptyStarsAsString}</p>
         </div>
       </Host>
     );
